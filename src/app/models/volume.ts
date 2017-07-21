d6a3fdf2f815226678254655deb406f033b74896
jecourtney <JCourtney@unitedmcgill.com>
2017-07-21 08:23:21 -0400

    Done with tools

--- src/app/models/volume.ts
+++ src/app/models/volume.ts
@@ -15,3 +15,46 @@ export interface IWtVol {
     percentH2OOut : number // out
 
 }
+
+export interface IFlowRate {
+    gasFlowRate : number,
+    flowInputUOM : string,
+    flowPercentH2OIn : number, // if !lb/hr
+    gasTemperature : number, // if !lb/hr
+    gasPressure : number, // if !lb/hr
+    waterMassRate : number, // if lb/hr !!
+    dryGasMolecularWeight : number, // if lb/hr !!
+    gasFlowRateResult : number, // out
+    initialWaterVapor : number, // out
+    percentVolumeH2O : number, // out
+    gasFlowRateResultUOM : string //out
+}
+
+export interface IPollute {
+    volumeFlow : number, 
+    pollutantMassFlow : number,
+    pollutantMolecularRate : number, // if ppmdv
+    pollutantConcentration : number, // out
+    pollutantConcentrationUOM : string
+}
+
+export interface IPressure {
+    elevation : number,
+    elevationPressureInHg : number,  //out in hg
+    elevationPressurePSIA : number // out psia
+}
+
+export type MoleWt = {
+    name: string,
+    mw: number,
+    percentVolume: number,
+    percentMass: number
+}
+
+export interface IMoleWt {
+    molWtInputs : MoleWt[],
+    totalVolumePercent : number, // out
+    totalMassPercent : number, // out
+    apparentMolecularWeight : number, // out
+    dryGasApparentMolecularWeight : number  // out
+}
--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -3,6 +3,11 @@ import { ToolsService } from './tools.service';
 import { IBlower } from '../models/blower';
 import { IRelHum } from '../models/volume';
 import { IWtVol } from '../models/volume';
+import { IFlowRate } from '../models/volume';
+import { IPollute } from '../models/volume';
+import { IPressure } from '../models/volume';
+import { IMoleWt } from '../models/volume';
+import { MoleWt } from '../models/volume';
 
 @Component({
   //selector: 'app-tools',
@@ -16,6 +21,17 @@ export class ToolsComponent implements OnInit {
   public H2OInputTypes = [
     { value: 'Weight', display: 'Weight'},
     { value: 'Volume', display: 'Volume'}
+  ];
+
+  public flowRateTypes = [
+    { value: 'SCFMd', display: 'SCFMd'},
+    { value: 'ACFM', display: 'ACFM'},
+    { value: 'lb/hr', display: 'lb/hr'}
+  ];
+
+  public polluteTypes = [
+    { value: 'gr/dscf', display: 'gr/dscf'},
+    { value: 'ppmdv', display: 'ppmdv'}
   ];
 
   public blower : IBlower = {
@@ -49,6 +65,43 @@ export class ToolsComponent implements OnInit {
     dryGasMolecularWeight : 0,
     percentH2OOut : 0 // out
   };
+  public flowRate : IFlowRate = {
+    gasFlowRate : 0,
+    flowInputUOM : this.flowRateTypes[0].value,
+    flowPercentH2OIn : 0, // if !lb/hr
+    gasTemperature : 0, // if !lb/hr
+    gasPressure : 0, // if !lb/hr
+    waterMassRate : 0, // if lb/hr !!
+    dryGasMolecularWeight : 0, // if lb/hr !!
+    gasFlowRateResult : 0, // out
+    initialWaterVapor : 0, // out
+    percentVolumeH2O : 0, // out   
+    gasFlowRateResultUOM : ''
+  };
+  public pollute : IPollute = {
+    volumeFlow : 0, 
+    pollutantMassFlow : 0,
+    pollutantMolecularRate : 0, // if ppmdv
+    pollutantConcentration : 0, // out
+    pollutantConcentrationUOM : this.polluteTypes[0].value
+  };
+  public pressure : IPressure = {
+    elevation : 0,
+    elevationPressureInHg : 0,  //out in hg
+    elevationPressurePSIA : 0 // out psia
+  };
+  public molWt : IMoleWt = {
+    molWtInputs : [
+    { name: 'CO\u2082', mw: 44.0096, percentVolume: 0, percentMass: 0 },
+    { name: 'N\u2082', mw: 28.0134, percentVolume: 0, percentMass: 0 }, { name: 'O\u2082', mw: 31.9989, percentVolume: 0, percentMass: 0 },
+    { name: 'SO\u2082', mw: 64.0644, percentVolume: 0, percentMass: 0 }, { name: 'HCl', mw: 36.4611, percentVolume: 0, percentMass: 0 },
+    { name: 'HF', mw: 20.0064, percentVolume: 0, percentMass: 0 }, { name: 'NO\u2082', mw: 46.0056, percentVolume: 0, percentMass: 0 },
+    { name: 'H\u2082O', mw: 18.0153, percentVolume: 0, percentMass: 0 }, { name: 'Other', mw: 0, percentVolume: 0, percentMass: 0 }],
+    totalVolumePercent : 0, // out
+    totalMassPercent : 0, // out
+    apparentMolecularWeight : 0, // out
+    dryGasApparentMolecularWeight : 0  // out    
+  }
 
   constructor( private toolsService: ToolsService) {
     //this.active = true;
@@ -126,6 +179,134 @@ export class ToolsComponent implements OnInit {
     } else {
         this.wtVol.percentH2OOut = Math.round(this.wtVol.percentH2OIn * 180000 / ((100 - this.wtVol.percentH2OIn) * this.wtVol.dryGasMolecularWeight + this.wtVol.percentH2OIn * 18)) / 100;
     }    
+  }
+
+  public convertFlowRates() {
+    if (this.flowRate.flowInputUOM === 'SCFMd') {
+        this.flowRate.gasFlowRateResult = Math.round(100 * this.flowRate.gasFlowRate * (460 + this.flowRate.gasTemperature) * 14.696 / ((1 - this.flowRate.flowPercentH2OIn / 100) * 530 * this.flowRate.gasPressure)) / 100;
+        this.flowRate.gasFlowRateResultUOM = 'ACFM';
+    } else if (this.flowRate.flowInputUOM === 'ACFM') {
+        this.flowRate.gasFlowRateResult = Math.round(100 * this.flowRate.gasFlowRate * (1 - this.flowRate.flowPercentH2OIn / 100) * 530 * this.flowRate.gasPressure / ((460 + this.flowRate.gasTemperature) * 14.696)) / 100;
+        this.flowRate.gasFlowRateResultUOM = 'SCFMd';
+    } else {
+        this.flowRate.gasFlowRateResult = Math.round(100 * this.flowRate.gasFlowRate * 10.73 * 530 / 14.696 / this.flowRate.dryGasMolecularWeight / 60) / 100;
+        this.flowRate.initialWaterVapor = Math.round(100 * this.flowRate.waterMassRate * 10.73 * 530 / 14.696 / 18 / 60) / 100;
+        this.flowRate.percentVolumeH2O = Math.round(10000 * this.flowRate.initialWaterVapor / (this.flowRate.gasFlowRateResult + this.flowRate.initialWaterVapor)) / 100;
+        this.flowRate.gasFlowRateResultUOM = 'SCFMd';
+    }    
+  }
+
+  public calculatePollutantConcentration() {
+    if (this.pollute.pollutantConcentrationUOM === 'gr/dscf') {
+        this.pollute.pollutantConcentration = Math.round(100 * this.pollute.pollutantMassFlow * 7000 / (60 * this.pollute.volumeFlow)) / 100;
+    } else {
+        this.pollute.pollutantConcentration = Math.round(100 * this.pollute.pollutantMassFlow * 6418091 / (this.pollute.volumeFlow * this.pollute.pollutantMolecularRate)) / 100;
+    }   
+  }
+
+  public calcPressureAtElevation(){
+    this.pressure.elevationPressurePSIA = Math.round(1469.6 * Math.exp(-0.00003540479 * this.pressure.elevation)) / 100;
+    this.pressure.elevationPressureInHg = Math.round(203.60206575996194 * this.pressure.elevationPressurePSIA) / 100;    
+  }
+
+  public updateMolecularWeights(nColumnChanged) {
+    this.molWt.apparentMolecularWeight = 0;
+    this.molWt.dryGasApparentMolecularWeight = 0;
+    this.molWt.totalVolumePercent = 0;
+    this.molWt.totalMassPercent = 0;
+    var totalMass = 0;
+    var h2oVolumePercent = 0;
+
+    if (nColumnChanged == 1) {
+        var dryTotalMass = 0;
+
+        // Sum total mass and total volume percent
+        for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+            // No negative percents
+            if (this.molWt.molWtInputs[i].percentVolume < 0) {
+                alert("Negative percents are not allowed.");
+                this.molWt.molWtInputs[i].percentVolume = 0;
+            }
+
+            // Enforce rule that volume total does not exceed 100%
+            if (this.molWt.totalVolumePercent + this.molWt.molWtInputs[i].percentVolume > 100) {
+                alert("Total volume percent cannot exceed 100.");
+                this.molWt.molWtInputs[i].percentVolume = 100 - this.molWt.totalVolumePercent;
+            }
+
+            this.molWt.totalVolumePercent += this.molWt.molWtInputs[i].percentVolume;
+            totalMass += this.molWt.molWtInputs[i].mw * this.molWt.molWtInputs[i].percentVolume / 100;
+            if (this.molWt.molWtInputs[i].name === 'H\u2082O') {
+                h2oVolumePercent = this.molWt.molWtInputs[i].percentVolume;
+            }
+        }
+
+        // Calculate individual percent masses, sum total mass percent, and sum dry mass total
+        for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+            this.molWt.molWtInputs[i].percentMass = Math.round(100 * this.molWt.molWtInputs[i].percentVolume * this.molWt.molWtInputs[i].mw / totalMass) / 100;
+            this.molWt.totalMassPercent += this.molWt.molWtInputs[i].percentMass;
+
+            if (this.molWt.totalVolumePercent !== h2oVolumePercent && this.molWt.molWtInputs[i].name !== 'H\u2082O') {
+                dryTotalMass += this.molWt.molWtInputs[i].percentVolume / (this.molWt.totalVolumePercent - h2oVolumePercent) * this.molWt.molWtInputs[i].mw * 100;
+            }
+        }
+        this.molWt.apparentMolecularWeight = Math.round(totalMass * 100) / 100;
+        this.molWt.dryGasApparentMolecularWeight = Math.round(dryTotalMass) / 100;
+    } else if (nColumnChanged == 2) {
+        var molUnitTotal = 0;
+
+        for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+            // No negative percents
+            if (this.molWt.molWtInputs[i].percentMass < 0) {
+                alert("Negative percents are not allowed.");
+                this.molWt.molWtInputs[i].percentMass = 0;
+            }
+
+            // Enforce rule that mass total does not exceed 100%
+            if (this.molWt.totalMassPercent + this.molWt.molWtInputs[i].percentMass > 100) {
+                alert("Total mass percent cannot exceed 100.");
+                this.molWt.molWtInputs[i].percentMass = 100 - this.molWt.totalMassPercent;
+            }
+
+            if (this.molWt.molWtInputs[i].mw !== 0) {
+                molUnitTotal += this.molWt.molWtInputs[i].percentMass / this.molWt.molWtInputs[i].mw;
+            }
+            this.molWt.totalMassPercent += this.molWt.molWtInputs[i].percentMass;
+            if (this.molWt.molWtInputs[i].name === 'H\u2082O') {
+                h2oVolumePercent = this.molWt.molWtInputs[i].percentVolume;
+            }
+        }
+
+        for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+            if (this.molWt.molWtInputs[i].mw !== 0 && molUnitTotal !== 0) {
+                this.molWt.molWtInputs[i].percentVolume = Math.round(10000 * this.molWt.molWtInputs[i].percentMass / this.molWt.molWtInputs[i].mw / molUnitTotal) / 100;
+            }
+            this.molWt.totalVolumePercent += this.molWt.molWtInputs[i].percentVolume;
+        }
+
+        for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+            if (this.molWt.totalVolumePercent !== h2oVolumePercent && this.molWt.molWtInputs[i].name !== 'H\u2082O') {
+                totalMass += this.molWt.molWtInputs[i].percentVolume / (this.molWt.totalVolumePercent - h2oVolumePercent) * this.molWt.molWtInputs[i].mw * 100;
+            }
+        }
+
+        this.molWt.apparentMolecularWeight = Math.round(1 / molUnitTotal * 10000) / 100;
+        this.molWt.dryGasApparentMolecularWeight = Math.round(totalMass) / 100;
+    }
+
+  }
+
+  public clear(){
+    this.molWt.totalVolumePercent = 0;
+    this.molWt.totalMassPercent = 0;
+    this.molWt.apparentMolecularWeight = 0;
+    this.molWt.dryGasApparentMolecularWeight = 0;
+
+    for (var i = 0; i < this.molWt.molWtInputs.length; i++) {
+      this.molWt.molWtInputs[i].percentVolume = 0;
+      this.molWt.molWtInputs[i].percentMass = 0;
+    }
+    this.molWt.molWtInputs[this.molWt.molWtInputs.length-1].mw = 0;
   }
 
 }
