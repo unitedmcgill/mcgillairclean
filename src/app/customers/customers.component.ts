93010543e3d9e74454533118416d8d9de62f1409
jecourtney <JCourtney@unitedmcgill.com>
2017-07-24 10:07:59 -0400

    Finished

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -12,4 +12,26 @@ export class CustomersComponent implements OnInit {
   ngOnInit() {
   }
 
+  public customers = ['3M Company', 'ADM', 'AFG', 'American Combustion Industries', 'American Video', 'Anderson 2000, Inc.', 'Anderson Cyanamid',
+            'Archer Daniels Midland (ADM)', 'Asarco', 'Ashland Chemical', 'B.F. Goodrich', 'Ball Glass', 'Basic Environmental Engineering', 'Battelle Memorial Institute',
+            'Belden Brick', 'Birmingham Steel', 'Borg-Warner', 'Bowater', 'Brockway Glass Company, Inc.', 'Bunge Corporation', 'Burney Mountain Power',
+            'Canmet Energy Technologies', 'Cardinal Glass', 'Cargill', 'Caterpillar', 'Central Soya', 'CertainTeed', 'Champion', 'City of Hampton, VA', 'Collins Pine',
+            'Container Corporation of America', 'Corning', 'Cristalarias Toro', 'Dan River', 'Degussa Carbon Black Corporation', 'Delco', 'Delco-Remy (G.M.)', 'DuPont',
+            'Federal White Cement', 'Ferro', 'Fiberglass Industries (FGI)', 'Footner Forest Products', 'Firestone', 'Ford Motor Company', 'Fort Howard Paper', 'Fort James',
+            'Foster Forbes', 'Fostoria Glass', 'GAF', 'Gallo Glass', 'General Electric Company', 'General Motors', 'Geneva Steel', 'Georgia-Pacific Corporation',
+            'Gilman Paper', 'Granite City Steel', 'GTE', 'Guardian Glass', 'Hyundai', 'International Paper', 'Islip Resource Recovery Agency', 'James River Paper',
+            'Johns Manville', 'Juken Nissho Limited', 'Kimble Glass', 'Knauf Fiberglass', 'LaGloria Gas and Oil', 'Langboard', 'Leone Industries',
+            'Louisiana-Pacific Corporation', 'Lummis Crest, Inc.', 'MacMillan Bloedel, Inc.', 'Mantiwoc', 'Marvin Windows & Doors', 'Mead', 'Medtronic, Inc.',
+            'Merck & Company, Inc.', 'Merck Pharmaceutical', 'Metco', 'Microchip Technology, Inc.', 'Mideco Dust Control', 'Montgomery County', 'Monsanto', 'Motorola',
+            'Mt. Lassen Power', 'National Lead', 'New Hanover County', 'NGA Ceramics', 'Nissan Motor', 'North Carolina Finishing', 'North Slope Borough',
+            'Northern State Power Company', 'NSA', 'Oberlin College', 'Occidental Chemical', 'Ogden Martin Systems, Inc.', 'Osram-Sylvania', 'Oswego Corporation, NY',
+            'Owens Brockway', 'Owens Corning', 'Owens-Illinois', 'Procter & Gamble Company', 'Philips Compounds', 'Phillip Morris', 'Philips Lighting Company', 'Pilkington',
+            'Pope Douglas', 'Potlatch Corporation', 'PPG', 'Procter and Gamble', 'RCA', 'R.W. Taylor/Oneida County', 'Rocky Mountain Bottling Company', 'Rogers Corporation',
+            'S.D. Warren', 'Scott Paper Company', 'SCM Glidco Organics', 'Sherwin-Williams', 'Smurfit-Stone', 'Southeastern Ohio Training Center', 'Spang & Company',
+            'St. Regis Paper & Bag', 'State of North Dakota', 'State of Ohio', 'Tafisa', 'Techneglas', 'Temple-Inland, Inc.', 'Texas Instruments', 'Thomson Video', 'Tropicana',
+            'U.S. Steel', 'Union Camp Corporation', 'Uniroyal Chemical Company, Inc.', 'University of Minnesota', 'UPF', 'Vetrotex America', 'Vetrotex CertainTeed',
+            'Vicon Recovery Associates', 'Volvo Truck Corporation', 'Von Roll, Inc.', 'Waste Management', 'Westmoreland County', 'Weyerhaeuser Company',
+            'Williamette Industries, Inc.'
+        ];
+
 }
--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -12,4 +12,30 @@ export class ReferencesComponent implements OnInit {
   ngOnInit() {
   }
 
+  public links = 
+           [{ name: 'EPA Home Page', link: 'http://www.epa.gov/' },
+            { name: 'EPA Air Science', link: 'http://www2.epa.gov/science-and-technology/air-science' },
+            { name: 'EPA CFR Promulgated Test Methods (TM)', link: 'http://www.epa.gov/ttnemc01/promgate.html' },
+            { name: 'National Emission Standards for Hazardous Air Pollutants (NESHAP)', link: 'http://www.epa.gov/airtoxics/mactfnlalph.html' },
+            { name: 'EPA Laws and Regulations', link: 'http://www.epa.gov/lawsregs/' },
+            { name: 'EPA Text Documents', link: 'http://www.epa.gov/ttn' },
+            { name: 'EPA Air Toxics Network', link: 'http://www.epa.gov/ttn/atw/' },
+            { name: 'EPA Clean Air Technology Center', link: 'http://www.epa.gov/ttn/catc' },
+            { name: 'EPA Envirofacts Warehouse', link: 'http://www.epa.gov/enviro' },
+            { name: 'EPA Pollution Prevention Information', link: 'http://www.epa.gov/p2/' },
+            { name: 'EPA CHIEF Inventories and Emission Factors', link: 'http://www.epa.gov/ttn/chief/index.html' },
+            { name: 'Pacific Northwest Pollution Prevention Resource Center', link: 'http://www.pprc.org/' },
+            { name: 'EPA Office of Air and Radiation', link: 'http://www.epa.gov/oar' },
+            { name: 'EPA Office of Air Quality Planning and Standards', link: 'http://www.epa.gov/oar/oaqps' },
+            { name: 'EPA Office of Underground Storage Tanks (OUST)', link: 'http://www.epa.gov/OUST' },
+            { name: 'State and Regional Underground Storage Tanks Agencies', link: 'http://www.epa.gov/oust/wheruliv.htm' },
+            { name: 'EPA Superfund', link: 'http://www.epa.gov/superfund/index.htm' },
+            { name: 'EPA Brownfields', link: 'http://www.epa.gov/brownfields' },
+            { name: 'EPA Chemical Emergency Preparedness', link: 'http://www.epa.gov/emergencies/index.htm' },
+            { name: 'EPA Right to Know Information', link: 'http://www.epa.gov/epahome/r2k.htm' },
+            { name: 'EPA Office of Water Division', link: 'http://www.epa.gov/OWM' },
+            { name: 'EPA Environmental and Compliance History Online (ECHO)', link: 'http://www.epa.gov/echo' },
+            { name: 'EPA Air Data', link: 'http://www.epa.gov/airdata/' },
+            { name: 'EPA Watershed Information', link: 'http://cfpub.epa.gov/surf/locate/index.cfm' }];
+
 }
